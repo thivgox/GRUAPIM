303794f34941cd727b9a6f2548b8936a1f0b0a64
Thiago Moraes <81823495+thivgox@users.noreply.github.com>
2026-03-20 16:35:20 -0300

    Add files via upload

---
+++ GRUAPIM/Aula3/contacts-api/src/main/java/br/ifsp/contacts/GlobalExceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package br.ifsp.contacts.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+
+         FieldError error = ex.getBindingResult().getFieldErrors().get(0);
+        errorResponse.put("erro", error.getDefaultMessage());
+
+        return errorResponse;
+    }
+}
